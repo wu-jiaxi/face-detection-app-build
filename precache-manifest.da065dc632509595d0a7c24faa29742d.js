19e22bf5312160e9ceb96bd4bfe2734bea056985
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2023-09-28 15:39:29 -0500

    build

---
+++ precache-manifest.da065dc632509595d0a7c24faa29742d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff7ece3132a4723797567cf38060b47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b58a2dda04e2ed7c8bc",
+    "url": "/static/css/2.7e346843.chunk.css"
+  },
+  {
+    "revision": "d2d3ed811af17b12bf9c",
+    "url": "/static/css/main.31043216.chunk.css"
+  },
+  {
+    "revision": "3b58a2dda04e2ed7c8bc",
+    "url": "/static/js/2.13303d21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.13303d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2d3ed811af17b12bf9c",
+    "url": "/static/js/main.2cd3c2fd.chunk.js"
+  },
+  {
+    "revision": "f254f45876acd58f2b29",
+    "url": "/static/js/runtime-main.f3ecb140.js"
+  }
+]);
